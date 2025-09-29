90317bf1e3558871998b5640a56772c4e2a9b031
ceduboza <ceduboza@gmail.com>
2025-09-29 22:11:06 +0200

    Fix ICountryRepository definition

--- src/modules/Countries/domain/repositories/ICountryRepository.ts
+++ src/modules/Countries/domain/repositories/ICountryRepository.ts
@@ -1,9 +1,9 @@
 import type { ICountryNormalizer } from "../normalizers/ICountryNormalizer";
-import type { Country } from "../entities/Country";
+import type { ICountryUI } from "../entities/ICountryUI";
 
 export interface ICountryRepository {
     _normalizer: ICountryNormalizer;
 
-    getCountries(): Promise<Country[]>;
-    getCountriesByName(name: string): Promise<Country[]>;
+    getCountries(): Promise<ICountryUI[]>;
+    getCountriesByName(name: string): Promise<ICountryUI[]>;
 }
